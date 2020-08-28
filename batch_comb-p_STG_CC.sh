d3e46b51a1fc5f3ed7d0f1adf8da53b5bea9a3df
qserenali <qli2@its.jnj.com>
2020-08-27 23:57:53 -0400

    Update batch_comb-p_STG_CC.sh

--- batch_comb-p_STG_CC.sh
+++ batch_comb-p_STG_CC.sh
@@ -4,6 +4,7 @@ bedtools sort -i $DATA.txt >$DATA.sorted.bed
 mkdir $DATA
 for chr in `cut -f 1 ${DATA}.sorted.bed | sort | uniq`; do grep -w $chr $DATA.sorted.bed > $DATA/$chr.output.bed; done
 ../../scripts/batch_comb-p.pl $DATA
+#wait for the step above to complete before continuing on the next step
 cat $DATA/chr*.regions.sig.bed | awk -F "\t" '{if ($7 <0.05) {print $0}}' | awk -F "\t" '{if ($5 >= 3) {print $0}}' | sort -k1,1V -k2,2n -k3,3n > DMR.${ENDPOINT}.regions.sig.all.bed
 /Janssen/bix/pkgs/homer/bin/annotatePeaks.pl DMR.${ENDPOINT}.regions.sig.all.bed hg19 -annStats DMR.${ENDPOINT} -go go.${ENDPOINT} -genomeOntology genomeOntology.${ENDPOINT} > DMR.${ENDPOINT}.regions.sig.all.anno 2>homer_${ENDPOINT}_output
 cut -f 2-5,8,15-16,18-20 DMR.${ENDPOINT}.regions.sig.all.anno | awk '{if(NR==1){print $1"\t"$2"\t"$0} else {print $1"\t"$2-1"\t"$0}}'| cut -f 1-2,5- > DMR.${ENDPOINT}.regions.sig.all.anno.m
